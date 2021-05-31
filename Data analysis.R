9bedff3496a9a6c003ca359fdb86bd7a50572f57
Jan Ira Villacarlos <jmvillacarlos@up.edu.ph>
2021-06-01 00:27:47 +0800

    importind and inspecting data set

--- Data analysis.R
+++ Data analysis.R
@@ -3,3 +3,8 @@ library(readr)
 
 # importing data set
 cervical_cancer <- read_csv("kag_risk_factors_cervical_cancer.csv")
+
+# checking data set
+head(cervical_cancer)
+names(cervical_cancer)
+str(cervical_cancer)
